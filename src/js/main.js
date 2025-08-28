f542e7992013cb154c8bab8e6cb01b1daab6d312
yuliv <yuliv@YULI-LAP>
2025-08-27 20:24:14 -0600

    Validamos el valor del input name en el formulario

--- src/js/main.js
+++ src/js/main.js
@@ -5,14 +5,6 @@ console.log(document); //DOM Document Object Model
 console.log(window);
 
 function main(){
-    console.log("All html are loaded; The Document is fully loaded and parsed!"); 
-
-    var nombre = document.querySelector('input[name=nombre]'); 
-    var nombreByID = document.getElementById('nombre');
-    
-    console.log(nombre);
-    console.log(nombreByID); 
-    
     //Subscripciones a eventos
     var buttonSendInfo = document.querySelector('#sendInfo');
     if(buttonSendInfo){ //Validar que existe el botón enviar 
@@ -22,12 +14,40 @@ function main(){
 
 function sendInfo(){
     console.log('click');
-    //Validar la información de todo el formulario
+    /*La función send tiene la responsabilidad de enviar la información
+      pero tiene que validad la consistencia de la información*/
+
+    //Qué? Validar Formulario
+    //Cómo? Implementar un validador 
+    //Validar que existe el botón de enviar información 
+    /*1.- Obtener los datos/ inputs del formulario
+      2.- Validarlos
+      3.- Evaluar que hacer con el resultado de la validación IsValidForm true|false]*/
+
     var form = document.querySelector('.form');
     console.log(form); 
     if(form){
+        //1
         var formInputs = document.querySelectorAll("input"); 
         console.log(formInputs);
+        //Validar los inputs
+        //Validar Nombre: Debe tener por lo menos 3 caracteres y y no contener caracteres especiales
+        var inputName = form.querySelector('input[name=nombre]'); 
+        var isValidName = true; 
+        if(inputName){
+            var valueName = inputName.value;
+            if(valueName.length < 3)
+                isValidName = false; 
+            var containsSpecialChars = /[^a-zA-Z\s]/.test(valueName);
+            if(containsSpecialChars)
+                isValidName = false;
+
+            if(!isValidName){
+                inputName.classList.add('invalid-input'); 
+            }else{
+                inputName.classList.remove('invalid-input')
+            }
+        }
     }
 }
 
